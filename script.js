cfa49e4e092c4b18c75f42af1dcb6d64f578849f
suman <suman_aks2003@yahoo.com>
2023-04-10 19:19:45 +0100

    add closure in javascript

--- script.js
+++ script.js
@@ -36,10 +36,47 @@ console.log(animal);
 
 //  console.log(result);
 
-let myName = ()=>{
-    return "Hello";
+// let myName = ()=>{
+//     return "Hello";
+// }
+// let greeting = (sayHi ,name)=> {
+//     console.log(sayHi() + " " + name)
+// }
+// greeting(myName, "ThapaTechnical");
+
+
+// let perOne = (friend,callfrd)=>{
+//     console.log(` I am busy right now. I am talking to ${friend}, I will call you back. `)
+//     callfrd();
+// }
+
+// let perTwo = () => {
+//     console.log(`Hey, what's up. I call  back you.`)
+// }
+
+// perOne("Thapa",perTwo);
+// perTwo();
+
+
+//  closure  Function
+
+const interviewQuestions = (name) => {
+    return function (topic){
+        if(name ==="vinod"){
+            console.log(`Hi, ${name}. Plz explain what is ${topic}.`);
+        }else if(name ==="Thapa"){
+            console.log(`Hi, ${name}. Plz explain what is ${topic}.`);
+        }else if(name ==="Thapatechnical"){
+            console.log(`Hi, ${name}. Plz explain what is ${topic}.`);
+        }else {
+            console.log("Welcome to TT Company");
+        }
+    }
 }
-let greeting = (sayHi ,name)=> {
-    console.log(sayHi() + " " + name)
-}
-greeting(myName, "ThapaTechnical");
+// const candidates = interviewQuestions("vinod");
+// candidates("UI")
+// const candidates1 = interviewQuestions("Thapa");
+// candidates1("React")
+
+interviewQuestions("vinod")("UI");
+interviewQuestions("Thapa")("React");
